dd03d119d6c0918bf0ea585841b089596a52a892
Lester Ng Kim Ho <lesterng94@gmail.com>
2018-12-11 02:57:59 +0800

    Add controllers for driver and passenger blacklists

---
+++ Unicab.Api/Unicab.Api/Controllers/DriverBlacklistsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Unicab.Api.Contexts;
+using Unicab.Api.Models;
+
+namespace Unicab.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverBlacklistsController : ControllerBase
+    {
+        private readonly UsersContext _context;
+
+        public DriverBlacklistsController(UsersContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DriverBlacklists
+        [HttpGet]
+        public IEnumerable<DriverBlacklist> GetDriverBlacklists()
+        {
+            return _context.DriverBlacklists;
+        }
+
+        // GET: api/DriverBlacklists/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDriverBlacklist([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var driverBlacklist = await _context.DriverBlacklists.FindAsync(id);
+
+            if (driverBlacklist == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(driverBlacklist);
+        }
+
+        // PUT: api/DriverBlacklists/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDriverBlacklist([FromRoute] int id, [FromBody] DriverBlacklist driverBlacklist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != driverBlacklist.DriverBlacklistId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(driverBlacklist).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DriverBlacklistExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DriverBlacklists
+        [HttpPost]
+        public async Task<IActionResult> PostDriverBlacklist([FromBody] DriverBlacklist driverBlacklist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.DriverBlacklists.Add(driverBlacklist);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDriverBlacklist", new { id = driverBlacklist.DriverBlacklistId }, driverBlacklist);
+        }
+
+        // DELETE: api/DriverBlacklists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDriverBlacklist([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var driverBlacklist = await _context.DriverBlacklists.FindAsync(id);
+            if (driverBlacklist == null)
+            {
+                return NotFound();
+            }
+
+            _context.DriverBlacklists.Remove(driverBlacklist);
+            await _context.SaveChangesAsync();
+
+            return Ok(driverBlacklist);
+        }
+
+        private bool DriverBlacklistExists(int id)
+        {
+            return _context.DriverBlacklists.Any(e => e.DriverBlacklistId == id);
+        }
+    }
+}
---
+++ Unicab.Api/Unicab.Api/Controllers/PassengerBlacklistsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Unicab.Api.Contexts;
+using Unicab.Api.Models;
+
+namespace Unicab.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassengerBlacklistsController : ControllerBase
+    {
+        private readonly UsersContext _context;
+
+        public PassengerBlacklistsController(UsersContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PassengerBlacklists
+        [HttpGet]
+        public IEnumerable<PassengerBlacklist> GetPassengerBlacklists()
+        {
+            return _context.PassengerBlacklists;
+        }
+
+        // GET: api/PassengerBlacklists/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPassengerBlacklist([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var passengerBlacklist = await _context.PassengerBlacklists.FindAsync(id);
+
+            if (passengerBlacklist == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(passengerBlacklist);
+        }
+
+        // PUT: api/PassengerBlacklists/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPassengerBlacklist([FromRoute] int id, [FromBody] PassengerBlacklist passengerBlacklist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != passengerBlacklist.PassengerBlacklistId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(passengerBlacklist).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PassengerBlacklistExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PassengerBlacklists
+        [HttpPost]
+        public async Task<IActionResult> PostPassengerBlacklist([FromBody] PassengerBlacklist passengerBlacklist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.PassengerBlacklists.Add(passengerBlacklist);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPassengerBlacklist", new { id = passengerBlacklist.PassengerBlacklistId }, passengerBlacklist);
+        }
+
+        // DELETE: api/PassengerBlacklists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePassengerBlacklist([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var passengerBlacklist = await _context.PassengerBlacklists.FindAsync(id);
+            if (passengerBlacklist == null)
+            {
+                return NotFound();
+            }
+
+            _context.PassengerBlacklists.Remove(passengerBlacklist);
+            await _context.SaveChangesAsync();
+
+            return Ok(passengerBlacklist);
+        }
+
+        private bool PassengerBlacklistExists(int id)
+        {
+            return _context.PassengerBlacklists.Any(e => e.PassengerBlacklistId == id);
+        }
+    }
+}
