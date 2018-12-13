a612f6dee9845ecbc19ca4eb1141e6f21f1b5c3f
Lester Ng Kim Ho <lesterng94@gmail.com>
2018-12-13 09:57:29 +0800

    Revert model to previous version

--- Unicab.Api/Unicab.Api/Migrations/20181213015549_RevertOriginalModel.Designer.cs
+++ Unicab.Api/Unicab.Api/Migrations/20181213015549_RevertOriginalModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Unicab.Api.Contexts;
 
 namespace Unicab.Api.Migrations
 {
     [DbContext(typeof(UsersContext))]
-    partial class UsersContextModelSnapshot : ModelSnapshot
+    [Migration("20181213015549_RevertOriginalModel")]
+    partial class RevertOriginalModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
